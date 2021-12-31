fb565f08253c30aee88e6006eb008dbbebb61a23
Daniel Ancuta <ancutadaniel@gmail.com>
2021-12-31 13:21:58 +0200

    initial commit

---
+++ migrations/2_deploy_contracts.js
@@ -0,0 +1,12 @@
+truconst NFT = artifacts.require("NFT")
+
+module.exports = async function (deployer) {
+    const IPFS_IMAGE_METADATA_URI = `ipfs://QmQdPYTY8yArgVmMJK319e75rsi91bwtUF5JsSF9CLnEYe/`
+
+    await deployer.deploy(
+        NFT,
+        "Famous Paintings",
+        "PAINT",
+        IPFS_IMAGE_METADATA_URI
+    )
+}
